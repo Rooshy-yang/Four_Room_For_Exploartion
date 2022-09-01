a9d24516065b08896cca10ac1702795f36656fdd
yangrushuai <54839995+Rooshy-yang@users.noreply.github.com>
2022-09-02 00:18:36 +0800

    add plot.py

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dir = 'outputs/2022-09-01/{}/0.npy'
+dir_ = 'outputs/2022-09-01/{}/0.npy'
 names = ('ourc', 'baseline', 'ourd_state')
 
 
@@ -12,13 +12,14 @@ grid_height = 13
 f, ax = plt.subplots(ncols=len(names))
 
 for idx, name in enumerate(names):
-    state = np.load(dir.format(name)).astype(np.int32)
+    state = np.load(dir_.format(name)).astype(np.int32)
     print('computing ', name)
     position = np.zeros([grid_width, grid_height])
-    for _ , value in enumerate(state):
+    for _, value in enumerate(state):
         row = value // grid_height
         col = value % grid_height
         position[row, col] += 1
+    np.save('{}_position_data.npy'.format(name), position)
     sns.heatmap(position, ax=ax[idx])
 
 plt.show()
